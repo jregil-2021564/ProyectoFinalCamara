5174ff05e3665cdd53ebce6ffe729b5443735a3f
hsamayoa-2021462 <hsamayoa-2021462@kinal.edu.gt>
2026-03-07 12:53:29 -0600

    fi: Se actualizan rutas de trafico, para agregar la parte de hugo

--- ProyectoFinalCamara/src/trafico/trafico.routes.js
+++ ProyectoFinalCamara/src/trafico/trafico.routes.js
@@ -7,6 +7,10 @@ import {
     obtenerMultas,
     obtenerMultasPorPlaca,
     registrarVehiculo,
+    verMultasPorPlaca,
+    validarSaldo,
+    aumentarMultas,
+    pagarMulta,
 } from './trafico.controller.js';
 
 const router = Router();
@@ -18,5 +22,11 @@ router.post('/infracciones', registrarInfraccion);
 router.get('/multas', validateJWT, obtenerMultas);
 router.get('/multas/:placa', validateJWT, obtenerMultasPorPlaca);
 router.post('/vehiculos', validateJWT, registrarVehiculo);
+
+// Agregando parte Hugo Benjamín Samayoa Díaz - 2021462
+router.get('/buscar/:placa', validateJWT, verMultasPorPlaca);
+router.get('/validar-saldo/:multaId', validateJWT, validarSaldo);
+router.put('/aumentar-multas', validateJWT, aumentarMultas);
+router.post('/pagar/:multaId', validateJWT, pagarMulta);
 
 export default router;
