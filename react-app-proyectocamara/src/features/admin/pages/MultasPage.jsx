012d99e2f93a0342a7a207e09e5f600d4a9f6b8c
jregil-2021564 <jregil-2021564@kinal.edu.gt>
2026-06-04 18:54:23 -0600

    add: Se agrego el archivo MultasPage a la subcarpeta de pages

---
+++ react-app-proyectocamara/src/features/admin/pages/MultasPage.jsx
@@ -0,0 +1,9 @@
+import MultasTable from '../components/MultasTable'
+const MultasPage = () => (
+  <div className="page-content">
+    <p className="page-title">Infracciones y Multas</p>
+    <p className="page-subtitle">Consulta y gestiona todas las infracciones registradas</p>
+    <MultasTable />
+  </div>
+)
+export default MultasPage
